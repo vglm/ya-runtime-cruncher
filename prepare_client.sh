e5ffc9bc96e6318e875e54f3fa1c068ea2ae7803
Sieciech Czajka <sieciech@gmail.com>
2025-04-03 15:28:26 +0200

    Fixing playground?

--- prepare_client.sh
+++ prepare_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone https://github.com/vglm/crunch-on-golem.git
-(cd crunch-on-golem && git checkout plugin && npm install)
+(cd crunch-on-golem && git checkout 5a88dbb560e26fa3c547dde40df4d32b29306267 && npm install)
